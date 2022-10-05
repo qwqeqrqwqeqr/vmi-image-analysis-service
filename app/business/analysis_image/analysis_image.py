11bc7bd3f05e78eea1abc2d836ca5920040aee7a
qwqeqrqwqeqr <rhdtkdxor123@kyonggi.ac.kr>
2022-10-05 18:33:40 +0900

    feat: [analysis] update rule condition 규칙을 적용할 이미지들에게 조건을 줍니다.

--- app/business/analysis_image/analysis_image.py
+++ app/business/analysis_image/analysis_image.py
@@ -13,24 +13,31 @@ from business.analysis_image.rule.rule_8 import rule_8
 from business.analysis_image.rule.rule_9 import rule_9
 from model.evaluation import Evaluation
 
-#TODO 유사도를 충족한 문제들만 규칙을 검사한다.
-def analysis_image(image, evaluation_list):
-    evaluation_list[4] = map_result(4, rule_4(image.a_4))
-    evaluation_list[5] = map_result(5, rule_5(image.a_5))
-    evaluation_list[6] = map_result(6, rule_6(image.a_6))
-    evaluation_list[7] = map_result(7, rule_7(image.a_7))
-    evaluation_list[8] = map_result(8, rule_8(image.a_8))
-    evaluation_list[9] = map_result(9, rule_9(image.a_9))
-    evaluation_list[10] = map_result(10, rule_10(image.a_10))
-    evaluation_list[11] = map_result(11, rule_11(image.a_11))
-    evaluation_list[12] = map_result(12, rule_12(image.a_12))
-    evaluation_list[13] = map_result(13, rule_13(image.a_13))
-    evaluation_list[14] = map_result(14, rule_14(image.a_14))
-    evaluation_list[15] = map_result(15, rule_15(image.a_15))
-    evaluation_list[16] = map_result(16, rule_16(image.a_16))
-
+'''
+    유사도가 예측을 통과한 이미지들(1점) 만 규칙검사를 진행합니다.
+'''
+def analysis_image(image, evaluation_list: list[Evaluation]):
+    for i in range(4, 17):
+        if evaluation_list[i].score == 1:
+            evaluation_list[i] = map_result(i, select_image(i, image))
     return evaluation_list
 
 
 def map_result(number, result):
     return Evaluation(number, result[0], result[1])
+
+
+def select_image(number, image):
+    return {4: rule_4(image.a_4),
+            5: rule_5(image.a_5),
+            6: rule_6(image.a_6),
+            7: rule_7(image.a_7),
+            8: rule_8(image.a_8),
+            9: rule_9(image.a_9),
+            10: rule_10(image.a_10),
+            11: rule_11(image.a_11),
+            12: rule_12(image.a_12),
+            13: rule_13(image.a_13),
+            14: rule_14(image.a_14),
+            15: rule_15(image.a_15),
+            16: rule_16(image.a_16)}.get(number, "")
