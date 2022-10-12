32b8a40c3f863351e31d973eec2dc4afc3dfe1bb
qwqeqrqwqeqr <rhdtkdxor123@kyonggi.ac.kr>
2022-10-12 15:00:23 +0900

    refactor: [analysis] update router 이미지를 조회하는 blueprint 경로를 수정합니다.

--- app/router/__init__.py
+++ app/router/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
-from router import analysis, base, image
+from router import analysis, base
 
 app = Flask(__name__)
 
 
 app.register_blueprint(analysis.blue_print)
-app.register_blueprint(image.blue_print)
 app.register_blueprint(base.blue_print)
 
--- app/router/analysis.py
+++ app/router/analysis.py
@@ -6,6 +6,8 @@ from database.query.image import set_patient_image
 from model.api_response import APIResponse
 from util.file import save_files
 
+from database.query.image import get_patient_image
+
 blue_print = Blueprint("analysis", __name__, url_prefix="/analysis")
 
 
@@ -23,11 +25,31 @@ def analysis_image():
         # 클라이언트로 부터 받은 파일 저장
         save_files(files)
 
-        #경로를 저장하고 채점을 진행합니다.
+        # 경로를 저장하고 채점을 진행합니다.
         for evaluation_code in evaluation_code_list:
             set_patient_image(evaluation_code)
             evaluation_result = evaluation(evaluation_code)
-            result.append({'evaluationCode': evaluation_code, 'scoreList': evaluation_result[0], 'total': evaluation_result[2]})
+            result.append(
+                {'evaluationCode': evaluation_code, 'scoreList': evaluation_result[0], 'total': evaluation_result[2]})
 
         return jsonify(APIResponse("success", "200", "AI 이미지 분석을 완료 하였습니다.", result))
 
+
+@blue_print.route("/patient/image/list/all", methods=["GET"])
+def get_patient_image_list_all():
+    if request.method == 'GET':
+        params = request.get_json()
+
+        result = []
+        image_list = []
+
+        for evaluation_code in params['evaluationCodeList']:
+            number = 4
+            image_list.clear()
+            for image in  get_patient_image(evaluation_code).get_image_list():
+                image_list.append({'image': image, 'number': number})
+                number += 1
+
+            result.append({'evaluationCode': evaluation_code, 'imageList': image_list})
+
+    return jsonify(APIResponse("success", "200", "AI 이미지 분석을 완료 하였습니다.", result))
